c8be3576eff25bebdca1e1e7aae529829a1e241f
Gabriel Saiago <grsaiago@gmail.com>
2025-03-05 00:01:48 -0300

    fix(init): icmq_seq to -1 so it properly starts at 0

--- src/initialization.c
+++ src/initialization.c
@@ -4,6 +4,7 @@ void	initialize_program_conf(ProgramConf *conf) {
 	memset(conf, 0, sizeof(ProgramConf));
 	conf->continue_execution = true;
 	conf->ip_version = IPV4;
+	conf->msg_seq = -1;
 	conf->flags.packet_interval = 1;
 	conf->flags.linger = -1;
 	conf->flags.count = -1;
